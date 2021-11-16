076fe2ad499cf1825d2db35336deb58b7c41c048
Hans Lakos <hans.lakos@student.hs-rm.de>
2021-11-17 00:27:58 +0100

    Und alles andere was fehlt

--- src/main/java/de/hsrm/mi/swt/rheinmainadventure/entities/Benutzer.java
+++ src/main/java/de/hsrm/mi/swt/rheinmainadventure/entities/Benutzer.java
@@ -1,6 +1,11 @@
 package de.hsrm.mi.swt.rheinmainadventure.entities;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.hibernate.annotations.DynamicUpdate;
+
 
 import javax.persistence.*;
 import java.util.StringJoiner;
@@ -10,27 +15,38 @@ import java.util.StringJoiner;
  * Dummy Code, um die Verwendung der Data.sql zu zeigen. Nicht Übernehmen!
  */
 @Entity
+@Getter
+@Setter
+@ToString
+@Table(name = "benutzer")
+@DynamicUpdate
 public class Benutzer {
 
   @Id
-  @GeneratedValue(strategy = GenerationType.SEQUENCE)
+  @GeneratedValue(strategy = GenerationType.IDENTITY) /*Sequence For better performance*/
   private Long id;
 
   @Column(unique = true, nullable = false)
   private String benutzername;
 
+  @Column
+  private boolean online;
+
   @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
   @Column(nullable = false)
   private String passwort;
 
-  public Benutzer() {
+  public Benutzer(){
+
   }
 
-  public Benutzer(String benutzername, String passwort) {
+  public Benutzer(String benutzername, String passwort, boolean online) {
     this.benutzername = benutzername;
     this.passwort = passwort;
+    this.online = false;
   }
 
+  /**
   @Override
   public String toString() {
     return new StringJoiner(", ", Benutzer.class.getSimpleName() + "[", "]")
@@ -38,6 +54,7 @@ public class Benutzer {
         .add("passwort='" + passwort + "'")
         .toString();
   }
+  **/
 
   /**
    * Zwei Nutzer sind dann gleich, wenn Sie den gleichen Nutzernamen haben.
@@ -58,29 +75,5 @@ public class Benutzer {
   @Override
   public int hashCode() {
     return benutzername.hashCode();
-  }
-
-  public String getBenutzername() {
-    return benutzername;
-  }
-
-  public void setBenutzername(String benutzername) {
-    this.benutzername = benutzername;
-  }
-
-  public long getId() {
-    return id;
-  }
-
-  public void setId(long id) {
-    this.id = id;
-  }
-
-  public String getPasswort() {
-    return passwort;
-  }
-
-  public void setPasswort(String passwort) {
-    this.passwort = passwort;
   }
 }
--- src/main/java/de/hsrm/mi/swt/rheinmainadventure/repositories/IntBenutzerRepo.java
+++ src/main/java/de/hsrm/mi/swt/rheinmainadventure/repositories/IntBenutzerRepo.java
@@ -1,2 +1,10 @@
-package de.hsrm.mi.swt.rheinmainadventure.repositories;public interface IntBenutzerRepo {
+package de.hsrm.mi.swt.rheinmainadventure.repositories;
+
+import de.hsrm.mi.swt.rheinmainadventure.entities.Benutzer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface IntBenutzerRepo extends JpaRepository<Benutzer, Long> { /*JPA extends CRUD repository in batch deleting etc*/
+    Benutzer findByBenutzername(String benutzername);
 }
