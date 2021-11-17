2f21bd158b43fa279621ec48bb9b108225033a23
Andreas Leipe <andi.leipe@yahoo.de>
2021-11-17 16:54:50 +0100

    #23 generierte Lobby-ID hinzugefügt

--- src/main/java/de/hsrm/mi/swt/rheinmainadventure/services/LobbyService.java
+++ src/main/java/de/hsrm/mi/swt/rheinmainadventure/services/LobbyService.java
@@ -1,9 +1,10 @@
 package de.hsrm.mi.swt.rheinmainadventure.services;
 
 import java.util.ArrayList;
-
+import java.util.List;
+import java.util.Collections;
+import java.util.Arrays;
 import org.springframework.stereotype.Service;
-
 import de.hsrm.mi.swt.rheinmainadventure.model.Lobby;
 import de.hsrm.mi.swt.rheinmainadventure.model.Player;
 
@@ -12,13 +13,51 @@ public class LobbyService {
 
     ArrayList<Lobby> lobbies = new ArrayList<Lobby>();
 
+    public String GenerateLobbyID(String benutzerName){
+
+        String lobbyID = "";
+        
+        // zufällig gemischter Benutzername
+        List<String> buchstaben = Arrays.asList(benutzerName.split(""));
+        Collections.shuffle(buchstaben);
+        String zufallsName = "";
+        for (String buchstabe : buchstaben) {
+          zufallsName += buchstabe;
+        }
+    
+        // Hash-Wert aus aktueller Zeit
+        String aktZeit = java.time.LocalTime.now().toString();
+        String zeitHashWert = String.valueOf(Math.abs(aktZeit.hashCode()));
+        
+        int zaehler = 0;
+        for(int i = 0; i < 10; i++){
+            if(i % 2 == 0){
+              if(zeitHashWert.length() > zaehler){
+                lobbyID += zeitHashWert.charAt(zaehler);
+                zaehler++;
+              }
+            }else{
+              if(zufallsName.length() > zaehler && Character.isLetterOrDigit(zufallsName.charAt(zaehler))){
+                lobbyID += zufallsName.charAt(zaehler);
+                zaehler++;
+              }
+            }
+        }
+    
+        return lobbyID;
+    }
+
     public Lobby createLobby() {
-        // TODO random ID generieren
-        Player host = new Player(0, "Player1");
+
+        // TODO Name vom Spieler bekommen
+        String spielerName = "Player1";
+
+        Player host = new Player(0, spielerName);
         ArrayList<Player> players = new ArrayList<Player>();
         players.add(host);
 
-        Lobby lobby = new Lobby("iwbvüubv+uq", players, host);
+        String lobbyID = GenerateLobbyID(spielerName);
+        Lobby lobby = new Lobby(lobbyID, players, host);
 
         lobbies.add(lobby);
 
