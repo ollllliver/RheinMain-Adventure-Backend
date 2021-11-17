2a555e5ea735e73919ab96bc0015dae979cd8175
Chand Mandru <chand.mandru@student.hs-rm.de>
2021-11-17 14:44:11 +0100

    Vorbereitung für Lobby-Join restriktionen

--- src/main/java/de/hsrm/mi/swt/rheinmainadventure/model/Lobby.java
+++ src/main/java/de/hsrm/mi/swt/rheinmainadventure/model/Lobby.java
@@ -6,11 +6,17 @@ public class Lobby {
     private String lobbyID;
     private ArrayList<Player> playerList;
     private Player host;
+    private boolean istVoll;
+    private boolean istGestartet;
+    private int spielerlimit = 0;
+
 
     public Lobby(String lobbyID, ArrayList<Player> playerList, Player host) {
         this.lobbyID = lobbyID;
         this.playerList = playerList;
         this.host = host;
+        this.istVoll = false;
+        this.istGestartet = false;
     }
 
     public ArrayList<Player> getPlayerList() {
@@ -19,6 +25,21 @@ public class Lobby {
 
     public void setPlayerList(ArrayList<Player> playerList) {
         this.playerList = playerList;
+    }
+
+    public boolean getIstVoll(){
+        return this.istVoll;
+    }
+
+    public void setIstVoll(boolean istVollNeu){
+        this.istVoll = istVollNeu;
+    }
+
+    public boolean getIstGestartet(){
+        return this.istGestartet;
+    }
+    public void setIstGestartet(boolean istGestartetNeu){
+        this.istGestartet = istGestartetNeu;
     }
 
     public String getlobbyID() {
@@ -36,5 +57,15 @@ public class Lobby {
     public void setHost(Player host) {
         this.host = host;
     }
+
+    public int getSpielerlimit() {
+        return spielerlimit;
+    }
+
+    public void setSpielerlimit(int spielerlimit) {
+        this.spielerlimit = spielerlimit;
+    }
+
+    
 
 }
