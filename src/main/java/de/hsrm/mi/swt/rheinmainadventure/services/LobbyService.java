6dbb1068cdb5e06fbc2fd90a378f19f5cec33ab4
Raoul Sudiana <Raoul.Sudiana@student.hs-rm.de>
2021-11-21 21:35:06 +0100

    added test folder

--- src/main/java/de/hsrm/mi/swt/rheinmainadventure/services/LobbyService.java
+++ src/main/java/de/hsrm/mi/swt/rheinmainadventure/services/LobbyService.java
@@ -1,4 +1,4 @@
-package de.hsrm.mi.swt.rheinmainadventure.services;
+package de.hsrm.mi.swt.rheinmainadventure.Services;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -15,86 +15,87 @@ import org.slf4j.LoggerFactory;
 @Service
 public class LobbyService {
 
-    ArrayList<Lobby> lobbies = new ArrayList<Lobby>();
-    Logger lg = LoggerFactory.getLogger(LobbyService.class);
+  ArrayList<Lobby> lobbies = new ArrayList<Lobby>();
+  Logger lg = LoggerFactory.getLogger(LobbyService.class);
 
-    public String GenerateLobbyID(String benutzerName){
+  public String GenerateLobbyID(String benutzerName) {
 
-        String lobbyID = "";
-        
-        // zufällig gemischter Benutzername
-        List<String> buchstaben = Arrays.asList(benutzerName.split(""));
-        Collections.shuffle(buchstaben);
-        String zufallsName = "";
-        for (String buchstabe : buchstaben) {
-          zufallsName += buchstabe;
+    String lobbyID = "";
+
+    // zufällig gemischter Benutzername
+    List<String> buchstaben = Arrays.asList(benutzerName.split(""));
+    Collections.shuffle(buchstaben);
+    String zufallsName = "";
+    for (String buchstabe : buchstaben) {
+      zufallsName += buchstabe;
+    }
+
+    // Hash-Wert aus aktueller Zeit
+    String aktZeit = java.time.LocalTime.now().toString();
+    String zeitHashWert = String.valueOf(Math.abs(aktZeit.hashCode()));
+
+    int zaehler = 0;
+    for (int i = 0; i < 10; i++) {
+      if (i % 2 == 0) {
+        if (zeitHashWert.length() > zaehler) {
+          lobbyID += zeitHashWert.charAt(zaehler);
+          zaehler++;
         }
-    
-        // Hash-Wert aus aktueller Zeit
-        String aktZeit = java.time.LocalTime.now().toString();
-        String zeitHashWert = String.valueOf(Math.abs(aktZeit.hashCode()));
-        
-        int zaehler = 0;
-        for(int i = 0; i < 10; i++){
-            if(i % 2 == 0){
-              if(zeitHashWert.length() > zaehler){
-                lobbyID += zeitHashWert.charAt(zaehler);
-                zaehler++;
-              }
-            }else{
-              if(zufallsName.length() > zaehler && Character.isLetterOrDigit(zufallsName.charAt(zaehler))){
-                lobbyID += zufallsName.charAt(zaehler);
-                zaehler++;
-              }
-            }
+      } else {
+        if (zufallsName.length() > zaehler && Character.isLetterOrDigit(zufallsName.charAt(zaehler))) {
+          lobbyID += zufallsName.charAt(zaehler);
+          zaehler++;
         }
-    
-        return lobbyID;
+      }
     }
 
-    public Lobby createLobby() {
+    return lobbyID;
+  }
 
-        //TODO : Name vom Spieler bekommen
-        String spielerName = "Player1";
+  public Lobby createLobby() {
 
-        Player host = new Player(0, spielerName);
-        ArrayList<Player> players = new ArrayList<Player>();
-        players.add(host);
+    // TODO : Name vom Spieler bekommen
+    String spielerName = "Player1";
 
-        String lobbyID = GenerateLobbyID(spielerName);
-        Lobby lobby = new Lobby(lobbyID, players, host,this);
-        lg.info("Lobby mit Lobby ID :"+lobby.getlobbyID()+" wurde erstellt.");
-        lobbies.add(lobby);
+    Player host = new Player(0, spielerName);
+    ArrayList<Player> players = new ArrayList<Player>();
+    players.add(host);
 
-        return lobby;
+    String lobbyID = GenerateLobbyID(spielerName);
+    Lobby lobby = new Lobby(lobbyID, players, host, this);
+    lg.info("Lobby mit Lobby ID :" + lobby.getlobbyID() + " wurde erstellt.");
+    lobbies.add(lobby);
+
+    return lobby;
+  }
+
+  public ArrayList<Lobby> getLobbies() {
+    return this.lobbies;
+  }
+
+  public Lobby getLobbyById(String Id) {
+    for (Lobby currLobby : lobbies) {
+      if (currLobby.getlobbyID().equals(Id)) {
+        return currLobby;
+      }
     }
+    return null;
+  }
 
-    public ArrayList<Lobby> getLobbies() {
-        return this.lobbies;
-    }
+  public void deleteLobbyById(String Id) {
 
-    public Lobby getLobbyById(String Id){
-        for(Lobby currLobby : lobbies){
-            if(currLobby.getlobbyID().equals(Id)){
-                return currLobby;
-            }
-        }
-        return null;
-    }
+    new Thread(() -> {
 
-    public void deleteLobbyById(String Id){
-
-      new Thread(() -> {
-        
       Lobby zuEntfernendeLobby = null;
-      for(Lobby currLobby : lobbies){
-        if(currLobby.getlobbyID() == Id){
+      for (Lobby currLobby : lobbies) {
+        if (currLobby.getlobbyID() == Id) {
           zuEntfernendeLobby = currLobby;
         }
       }
-      
-      if (zuEntfernendeLobby!=null) lobbies.remove(zuEntfernendeLobby);
+
+      if (zuEntfernendeLobby != null)
+        lobbies.remove(zuEntfernendeLobby);
 
     }).start();
-    }
+  }
 }
