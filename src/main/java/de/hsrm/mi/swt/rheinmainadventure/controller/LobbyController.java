9399f0bd63be2d468e49fa78bc397033ac853635
Raoul Sudiana <Raoul.Sudiana@student.hs-rm.de>
2021-11-21 21:59:06 +0100

    fixed some test problems

--- src/main/java/de/hsrm/mi/swt/rheinmainadventure/controller/LobbyController.java
+++ src/main/java/de/hsrm/mi/swt/rheinmainadventure/controller/LobbyController.java
@@ -17,34 +17,35 @@ import de.hsrm.mi.swt.rheinmainadventure.entities.Benutzer;
 import de.hsrm.mi.swt.rheinmainadventure.model.Lobby;
 import de.hsrm.mi.swt.rheinmainadventure.services.LobbyService;
 
-
 @Controller
 class LobbyController {
 
     @Autowired
     private LobbyService lobbyService;
-    /* Pseudo BenutzerService zum accessen der Datenbank um nutzernamen abzugleichen und abzufragen
-     @Autowired
-     private BenutzerService benutzerService;   
-    */
+    /*
+     * Pseudo BenutzerService zum accessen der Datenbank um nutzernamen abzugleichen
+     * und abzufragen
+     * 
+     * @Autowired private BenutzerService benutzerService;
+     */
 
     Logger lg = LoggerFactory.getLogger(LobbyController.class);
 
     // LobbyService injected and used as instance
-    public LobbyController(){
+    public LobbyController() {
     }
 
     @MessageMapping("/topic/lobby/create")
     @SendTo("/topic/lobby/create")
     public String createLobby(String msg) throws Exception {
         lg.info(msg);
-        //Lobby Initialisieren und dem LobbyService in der LobbyListe hinzufügen
+        // Lobby Initialisieren und dem LobbyService in der LobbyListe hinzufügen
         lobbyService.createLobby();
         ObjectMapper objectMapper = new ObjectMapper();
         return objectMapper.writeValueAsString(lobbyService.getLobbies());
     }
 
-    //Initialisiert das Starten einer Lobby also von Lobby -> Interner Spielwechsel
+    // Initialisiert das Starten einer Lobby also von Lobby -> Interner Spielwechsel
     @MessageMapping("/lobby/{lobbyId}/start")
     @SendTo("/topic/lobby/started")
     public int startGame(@PathVariable String lobbyId) throws Exception {
@@ -55,42 +56,48 @@ class LobbyController {
         return lobbyStartCountdown;
     }
 
-    //Unter der nutzung eines Lobby-Links wird ein nutzer der jeweiligen Lobby zugewiesen.
+    // Unter der nutzung eines Lobby-Links wird ein nutzer der jeweiligen Lobby
+    // zugewiesen.
     @GetMapping("/lobby/{lobbyId}")
-    public void lobbieBeitretenMitLink(@PathVariable String lobbyId,@SessionAttribute("username") String username){
+    public void lobbieBeitretenMitLink(@PathVariable String lobbyId, @SessionAttribute("username") String username) {
         Lobby currLobby = lobbyService.getLobbyById(lobbyId);
-        if(!currLobby.getIstGestartet() && !currLobby.getIstVoll()){
+        if (!currLobby.getIstGestartet() && !currLobby.getIstVoll()) {
             /*
-            Legt benutzer Instanz an wenn man einen User mit dem aktuellen Session Benutzernamen findet.
-            Benutzer tempNutzer = benutzerService.getBenutzerByUsername(username); */
+             * Legt benutzer Instanz an wenn man einen User mit dem aktuellen Session
+             * Benutzernamen findet. Benutzer tempNutzer =
+             * benutzerService.getBenutzerByUsername(username);
+             */
 
-            Benutzer testNutzer = new Benutzer("jerry","testy");
+            Benutzer testNutzer = new Benutzer("jerry", "testy");
 
-            //Unter der Kondition dass das Spielerlimit noch nicht erreicht wurde, wird ein neuer Spieler hinzugefügt.
+            // Unter der Kondition dass das Spielerlimit noch nicht erreicht wurde, wird ein
+            // neuer Spieler hinzugefügt.
             currLobby.nutzerHinzufuegen(testNutzer);
         }
     }
 
-    //Post Mapping auf die seite wo sich der Random-Join Button befindet.
+    // Post Mapping auf die seite wo sich der Random-Join Button befindet.
     @PostMapping("/lobby")
-    public void lobbieBeitretenZufaellig(@SessionAttribute("username") String username){
+    public void lobbieBeitretenZufaellig(@SessionAttribute("username") String username) {
 
         Lobby tempLobby = null;
         for (Lobby currLobby : lobbyService.getLobbies()) {
-            if(!currLobby.getIstGestartet() && !currLobby.getIstVoll() && !currLobby.getIstPrivat()){
+            if (!currLobby.getIstGestartet() && !currLobby.getIstVoll() && !currLobby.getIstPrivat()) {
                 tempLobby = currLobby;
                 break;
             }
         }
         /*
-        Legt benutzer Instanz an wenn man einen User mit dem aktuellen Session Benutzernamen findet.
-        Benutzer tempNutzer = benutzerService.getBenutzerByUsername(username); */
-        Benutzer testNutzer = new Benutzer("jerry","testy");
-        if (tempLobby!=null){
+         * Legt benutzer Instanz an wenn man einen User mit dem aktuellen Session
+         * Benutzernamen findet. Benutzer tempNutzer =
+         * benutzerService.getBenutzerByUsername(username);
+         */
+        Benutzer testNutzer = new Benutzer("jerry", "testy");
+        if (tempLobby != null) {
             tempLobby.nutzerHinzufuegen(testNutzer);
-        }else{
+        } else {
             lg.info("Ungueltige Lobby ID wurde angegeben.");
-            //TODO : Redirect zur Homepage und Fehlermeldung Popup mit dem Logger Infotext.
+            // TODO : Redirect zur Homepage und Fehlermeldung Popup mit dem Logger Infotext.
         }
     }
 
--- src/main/java/de/hsrm/mi/swt/rheinmainadventure/services/LobbyService.java
+++ src/main/java/de/hsrm/mi/swt/rheinmainadventure/services/LobbyService.java
@@ -1,4 +1,4 @@
-package de.hsrm.mi.swt.rheinmainadventure.Services;
+package de.hsrm.mi.swt.rheinmainadventure.services;
 
 import java.util.ArrayList;
 import java.util.Arrays;
