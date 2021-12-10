3fb621d49a03097336ec2e260c4788dddd6150fa
Friedrich Roskosch <Friedrich.Roskosch@student.hs-rm.de>
2021-12-10 20:34:03 +0100

    🚨 Von mir damals verzapfte Formatierung korrigiert

--- src/main/java/de/hsrm/mi/swt/rheinmainadventure/entities/Benutzer.java
+++ src/main/java/de/hsrm/mi/swt/rheinmainadventure/entities/Benutzer.java
@@ -2,113 +2,101 @@ package de.hsrm.mi.swt.rheinmainadventure.entities;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import javax.persistence.*;
-
 import com.fasterxml.jackson.annotation.JsonProperty.Access;
+
+import javax.persistence.*;
 
 /**
  * Benutzer-Entity für das Benutzer-Repository
  */
 @JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
 @Entity
-@Table(name="benutzer")
+@Table(name = "benutzer")
 public class Benutzer {
 
-  //automatisch generierte ID
-  @Id
-  @GeneratedValue
-  private Long id;
-  //Spalte Loginname im Repository
-  @Column(name="BENUTZERNAME", unique=true)
-  private String benutzername;
+    //automatisch generierte ID
+    @Id
+    @GeneratedValue
+    private Long id;
+    //Spalte Loginname im Repository
+    @Column(name = "BENUTZERNAME", unique = true)
+    private String benutzername;
 
-  private String passwort;
+    private String passwort;
 
-  //automatisch generierte Versionsnummer
-  @Version
-  @GeneratedValue
-  @JsonProperty(access=Access.WRITE_ONLY)
-  private Long version;
+    //automatisch generierte Versionsnummer
+    @Version
+    @GeneratedValue
+    @JsonProperty(access = Access.WRITE_ONLY)
+    private Long version;
 
-  //Getter/Setter/hashCode/equals/toString
-  public String getBenutzername() {
-    return benutzername;
-  }
+    //Getter/Setter/hashCode/equals/toString
+    public String getBenutzername() {
+        return benutzername;
+    }
 
-  public void setBenutzername(String loginname) {
-    this.benutzername = loginname;
-  }
+    public void setBenutzername(String loginname) {
+        this.benutzername = loginname;
+    }
 
-  public String getPasswort() {
-    return passwort;
-  }
+    public String getPasswort() {
+        return passwort;
+    }
 
-  public void setPasswort(String passwort) {
-    this.passwort = passwort;
-  }
+    public void setPasswort(String passwort) {
+        this.passwort = passwort;
+    }
 
-  public Long getId() {
-    return id;
-  }
+    public Long getId() {
+        return id;
+    }
 
-  public void setId(Long id) {
-    this.id = id;
-  }
+    public void setId(Long id) {
+        this.id = id;
+    }
 
-  public Long getVersion() {
-    return version;
-  }
+    public Long getVersion() {
+        return version;
+    }
 
-  public void setVersion(Long version) {
-    this.version = version;
-  }
+    public void setVersion(Long version) {
+        this.version = version;
+    }
 
-  @Override
-  public int hashCode() {
-    final int prime = 31;
-    int result = 1;
-    result = prime * result + ((id == null) ? 0 : id.hashCode());
-    result = prime * result + ((benutzername == null) ? 0 : benutzername.hashCode());
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        result = prime * result + ((benutzername == null) ? 0 : benutzername.hashCode());
 
-    result = prime * result + ((passwort == null) ? 0 : passwort.hashCode());
-    result = prime * result + ((version == null) ? 0 : version.hashCode());
-    return result;
-  }
+        result = prime * result + ((passwort == null) ? 0 : passwort.hashCode());
+        result = prime * result + ((version == null) ? 0 : version.hashCode());
+        return result;
+    }
 
-  @Override
-  public boolean equals(Object obj) {
-    if (this == obj)
-      return true;
-    if (obj == null)
-      return false;
-    if (getClass() != obj.getClass())
-      return false;
-    Benutzer other = (Benutzer) obj;
-    if (id == null) {
-      if (other.id != null)
-        return false;
-    } else if (!id.equals(other.id))
-      return false;
-    if (benutzername == null) {
-      if (other.benutzername != null)
-        return false;
-    } else if (!benutzername.equals(other.benutzername))
-    if (passwort == null) {
-      if (other.passwort != null)
-        return false;
-    } else if (!passwort.equals(other.passwort))
-      return false;
-    if (version == null) {
-      if (other.version != null)
-        return false;
-    } else if (!version.equals(other.version))
-      return false;
-    return true;
-  }
 
-  @Override
-  public String toString() {
-    return "Benutzer [id=" + id + ", benutzername=" + benutzername
-            + ", passwort=" + passwort + ", version=" + version + "]";
-  }
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null) return false;
+        if (getClass() != obj.getClass()) return false;
+        Benutzer other = (Benutzer) obj;
+        if (id == null) {
+            if (other.id != null) return false;
+        } else if (!id.equals(other.id)) return false;
+        if (benutzername == null) {
+            if (other.benutzername != null) return false;
+        } else if (!benutzername.equals(other.benutzername)) if (passwort == null) {
+            if (other.passwort != null) return false;
+        } else if (!passwort.equals(other.passwort)) return false;
+        if (version == null) {
+            return other.version == null;
+        } else return version.equals(other.version);
+    }
+
+    @Override
+    public String toString() {
+        return "Benutzer [id=" + id + ", benutzername=" + benutzername + ", passwort=" + passwort + ", version=" + version + "]";
+    }
 }
