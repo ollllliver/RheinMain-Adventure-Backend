4167087d0d698d1d4da9d67173d408ab8382d985
Raoul Sudiana <Raoul.Sudiana@student.hs-rm.de>
2021-12-13 20:11:29 +0100

    #81 SpielerKlasse als Teilnehemer

---
+++ src/main/java/de/hsrm/mi/swt/rheinmainadventure/spiel/Teilnehmer.java
@@ -0,0 +1,48 @@
+package de.hsrm.mi.swt.rheinmainadventure.spiel;
+
+public class Teilnehmer {
+    private String name;
+    private String avatar;
+    private int positionX;
+    private int positionY;
+
+    public Teilnehmer(String name, String avatar, int positionX, int positionY) {
+        this.name = name;
+        this.avatar = avatar;
+        this.positionX = positionX;
+        this.positionY = positionY;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAvatar() {
+        return avatar;
+    }
+
+    public void setAvatar(String avatar) {
+        this.avatar = avatar;
+    }
+
+    public int getPositionX() {
+        return positionX;
+    }
+
+    public void setPositionX(int positionX) {
+        this.positionX = positionX;
+    }
+
+    public int getPositionY() {
+        return positionY;
+    }
+
+    public void setPositionY(int positionY) {
+        this.positionY = positionY;
+    }
+
+}
