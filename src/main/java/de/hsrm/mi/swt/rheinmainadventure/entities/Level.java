a415356c8de8e1008cb00ea337c8f2fe2bcfb2b7
Friedrich Roskosch <Friedrich.Roskosch@student.hs-rm.de>
2021-12-15 16:50:41 +0100

    🚧 Level Entity + Repository angelegt

---
+++ src/main/java/de/hsrm/mi/swt/rheinmainadventure/entities/Level.java
@@ -0,0 +1,98 @@
+package de.hsrm.mi.swt.rheinmainadventure.entities;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+@Entity
+public class Level {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long levelId;
+
+    @Column(unique = true, nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private int minSpieler;
+
+    @Column(nullable = false)
+    private int maxSpieler;
+
+    @Version
+    private Long version;
+
+    @Override
+    public String toString() {
+        return "Level{" + "levelId=" + levelId +
+            ", name='" + name + '\'' +
+            ", minSpieler=" + minSpieler +
+            ", maxSpieler=" + maxSpieler +
+            ", version=" + version +
+            '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Level level = (Level) o;
+
+        if (getMinSpieler() != level.getMinSpieler()) return false;
+        if (getMaxSpieler() != level.getMaxSpieler()) return false;
+        if (!getLevelId().equals(level.getLevelId())) return false;
+        return getName().equals(level.getName());
+    }
+
+    @Override
+    public int hashCode() {
+        int result = getLevelId().hashCode();
+        result = 31 * result + getName().hashCode();
+        result = 31 * result + getMinSpieler();
+        result = 31 * result + getMaxSpieler();
+        return result;
+    }
+
+    public Long getLevelId() {
+        return levelId;
+    }
+
+    public void setLevelId(Long levelId) {
+        this.levelId = levelId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getMinSpieler() {
+        return minSpieler;
+    }
+
+    public void setMinSpieler(int minSpieler) {
+        this.minSpieler = minSpieler;
+    }
+
+    public int getMaxSpieler() {
+        return maxSpieler;
+    }
+
+    public void setMaxSpieler(int maxSpieler) {
+        this.maxSpieler = maxSpieler;
+    }
+
+    public Long getVersion() {
+        return version;
+    }
+
+    public void setVersion(Long version) {
+        this.version = version;
+    }
+}
