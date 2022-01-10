eb051915c201b204ba91fad7c96039131750278b
Julian Graeber <julian.graeber@student.hs-rm.de>
2022-01-10 11:55:19 +0100

    🚧 #120 (Hans) + #131 (Julian) - Karte wird eingereicht und testweise in der DB gespeichert

--- src/main/java/de/hsrm/mi/swt/rheinmainadventure/spiel/LevelServiceImpl.java
+++ src/main/java/de/hsrm/mi/swt/rheinmainadventure/spiel/LevelServiceImpl.java
@@ -2,7 +2,6 @@ package de.hsrm.mi.swt.rheinmainadventure.spiel;
 
 import de.hsrm.mi.swt.rheinmainadventure.benutzer.BenutzerService;
 import de.hsrm.mi.swt.rheinmainadventure.entities.*;
-import de.hsrm.mi.swt.rheinmainadventure.model.Element;
 import de.hsrm.mi.swt.rheinmainadventure.model.Position;
 import de.hsrm.mi.swt.rheinmainadventure.repositories.LevelRepository;
 import de.hsrm.mi.swt.rheinmainadventure.repositories.MobiliarRepository;
