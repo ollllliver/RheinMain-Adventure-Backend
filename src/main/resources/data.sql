3221bf68aa61c56e349bfc481ae64477d7fe809c
Timothy Tjong-Ayong <timothy.tjong-ayong@student.hs-rm.de>
2022-01-12 02:42:37 +0100

    #75 Tür, Schlüssel und Wand sind jetzt alles gleich große Blöcke

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -11,7 +11,7 @@ INSERT INTO raum (raum_id, raum_index, level_level_id)
 VALUES (20, 0, 1);
 
 INSERT INTO mobiliar (mobiliar_id, modelluri, name, mobiliartyp)
-VALUES (30, 'gltf/texturedBox_embedded/Box_regular.gltf', 'Normale Box', null);
+VALUES (30, 'gltf/texturedBox_embedded/dirt.gltf', 'Normale Box', null);
 
 INSERT INTO mobiliar (mobiliar_id, modelluri, name, mobiliartyp)
 VALUES (31, 'gltf/duck_embedded/Duck.gltf', 'Ente', 1);
@@ -22,17 +22,57 @@ VALUES (32, 'gltf/texturedBox_embedded/BoxInterleaved.gltf', 'Eingangs-Box', 0);
 INSERT INTO mobiliar (mobiliar_id, modelluri, name, mobiliartyp)
 VALUES (33, 'gltf/duck_embedded/Duck.gltf', 'Eingangs-Ente', 0);
 
-INSERT INTO raum_mobiliar (raum_mobiliar_id, positionx, positiony, mobiliar_id, raum_id)
-VALUES (40, 5, 5, 30, 20);
+INSERT INTO mobiliar (mobiliar_id, modelluri, name, mobiliartyp)
+VALUES (34, 'gltf/texturedBox_embedded/door.gltf', 'Tür', 0);
+
+INSERT INTO mobiliar (mobiliar_id, modelluri, name, mobiliartyp)
+VALUES (35, 'gltf/texturedBox_embedded/key.gltf', 'Schlüssel', 0);
+
 
 INSERT INTO raum_mobiliar (raum_mobiliar_id, positionx, positiony, mobiliar_id, raum_id)
-VALUES (41, 6, 6, 30, 20);
+VALUES (40, 5, 3, 30, 20);
 
 INSERT INTO raum_mobiliar (raum_mobiliar_id, positionx, positiony, mobiliar_id, raum_id)
-VALUES (42, 7, 7, 30, 20);
+VALUES (41, 4, 3, 30, 20);
 
 INSERT INTO raum_mobiliar (raum_mobiliar_id, positionx, positiony, mobiliar_id, raum_id)
-VALUES (43, 8, 8, 31, 20);
+VALUES (42, 3, 3, 30, 20);
 
 INSERT INTO raum_mobiliar (raum_mobiliar_id, positionx, positiony, mobiliar_id, raum_id)
-VALUES (44, 0, 6, 33, 20);
+VALUES (43, 2, 3, 30, 20);
+
+INSERT INTO raum_mobiliar (raum_mobiliar_id, positionx, positiony, mobiliar_id, raum_id)
+VALUES (44, 1, 3, 30, 20);
+
+INSERT INTO raum_mobiliar (raum_mobiliar_id, positionx, positiony, mobiliar_id, raum_id)
+VALUES (45, 0, 3, 30, 20);
+
+INSERT INTO raum_mobiliar (raum_mobiliar_id, positionx, positiony, mobiliar_id, raum_id)
+VALUES (46, 0, 0, 30, 20);
+
+INSERT INTO raum_mobiliar (raum_mobiliar_id, positionx, positiony, mobiliar_id, raum_id)
+VALUES (47, 1, 0, 30, 20);
+
+INSERT INTO raum_mobiliar (raum_mobiliar_id, positionx, positiony, mobiliar_id, raum_id)
+VALUES (48, 2, 0, 30, 20);
+
+INSERT INTO raum_mobiliar (raum_mobiliar_id, positionx, positiony, mobiliar_id, raum_id)
+VALUES (49, 3, 0, 34, 20);
+
+INSERT INTO raum_mobiliar (raum_mobiliar_id, positionx, positiony, mobiliar_id, raum_id)
+VALUES (53, 4, 0, 30, 20);
+
+INSERT INTO raum_mobiliar (raum_mobiliar_id, positionx, positiony, mobiliar_id, raum_id)
+VALUES (50, 5, 0, 30, 20);
+
+INSERT INTO raum_mobiliar (raum_mobiliar_id, positionx, positiony, mobiliar_id, raum_id)
+VALUES (51, 8, 8, 31, 20);
+
+INSERT INTO raum_mobiliar (raum_mobiliar_id, positionx, positiony, mobiliar_id, raum_id)
+VALUES (52, 0, 6, 33, 20);
+
+INSERT INTO raum_mobiliar (raum_mobiliar_id, positionx, positiony, mobiliar_id, raum_id)
+VALUES (54, 1, 1, 35, 20);
+
+INSERT INTO raum_mobiliar (raum_mobiliar_id, positionx, positiony, mobiliar_id, raum_id)
+VALUES (55, 1, 2, 35, 20);
